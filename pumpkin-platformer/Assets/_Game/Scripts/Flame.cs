1e79f7d732c5e2ab7c4f726a1e5d0c2fa05f47fc
Emir <emirbesirx@gmail.com>
2025-10-11 20:35:59 +0300

    feat: change skybox

--- pumpkin-platformer/Assets/_Game/Scripts/Flame.cs
+++ pumpkin-platformer/Assets/_Game/Scripts/Flame.cs
@@ -1,16 +1,39 @@
+using System;
 using UnityEngine;
 
 public class Flame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Flame Settings")]
+    [SerializeField] private float flameTotal = 120f;
+    [SerializeField] private float flameDecayRate = 1f;
+    [SerializeField] private Light flameLight;
+    
+    private float currentFlame;
+
+    public event Action OnFlameDepleted;
+
+    private void Start()
     {
-        
+        currentFlame = flameTotal;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DecayFlame();
+    }
+
+    private void DecayFlame()
+    {
+        currentFlame -= flameDecayRate * Time.deltaTime;
+
+        if (currentFlame <= 0)
+        {
+            OnFlameDepleted?.Invoke();
+        }
+    }
+    
+    private void UpdateLightIntensity()
+    {
+        flameLight.intensity = Mathf.Clamp(currentFlame / flameTotal, 0.1f, 1f);
     }
 }
